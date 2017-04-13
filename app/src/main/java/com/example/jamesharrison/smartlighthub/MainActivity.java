c00f9ffddb85ea6c4970755f310d3b87b531fc4d
James Harrison <jharrison23@knights.ucf.edu>
2017-04-13 16:18:19 -0400

    Added code to the main screen to navigate to the login and create user view

--- app/src/main/java/com/example/jamesharrison/smartlighthub/MainActivity.java
+++ app/src/main/java/com/example/jamesharrison/smartlighthub/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.jamesharrison.smartlighthub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+        TextView signUp = (TextView) findViewById(R.id.signUp);
+        signUp.setClickable(true);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

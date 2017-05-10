ffb5a8bd4b53b7f58600303c02600240fb75bd29
James Harrison <jharrison23@knights.ucf.edu>
2017-05-10 02:07:06 -0400

    User can create account with email and password

--- app/src/main/java/com/example/seniordesign/smartlighthub/CreateAccount.java
+++ app/src/main/java/com/example/seniordesign/smartlighthub/CreateAccount.java
@@ -1,17 +1,28 @@
 package com.example.seniordesign.smartlighthub;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateAccount extends AppCompatActivity {
 
+    private static final String TAG = "CreateAccount";
+
     private DatabaseReference mDatabase;
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     private Button createAccount;
     private EditText fullName, userName, email, password, confirmPassword;
@@ -28,8 +39,27 @@ public class CreateAccount extends AppCompatActivity {
 
     public void init()
     {
-        // Get a reference to the root directory of the application
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener()
+        {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
+            {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null)
+                {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                }
+
+                else
+                {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
 
         createAccount = (Button) findViewById(R.id.createAccountButton);
 
@@ -44,8 +74,42 @@ public class CreateAccount extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                mDatabase.child("User").setValue(fullName.getText().toString(), userName.getText().toString());
+                String emailAddress = email.getText().toString();
+
+                String pass = password.getText().toString();
+
+                if (!emailAddress.equals("") && !pass.equals(""))
+                {
+                    mAuth.createUserWithEmailAndPassword(emailAddress, pass);
+                    Toast.makeText(CreateAccount.this, "user Signed in", Toast.LENGTH_SHORT).show();
+                }
+
+                else
+                {
+                    Toast.makeText(CreateAccount.this, "Fill out email and password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        if (mAuthListener != null)
+        {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+
 }

aa03b824f433a0e3dc4ac85f8d3acc37ad9e03e0
James Harrison <jharrison23@knights.ucf.edu>
2017-05-24 19:10:04 -0400

    Added a logout button so the current user can log out

--- app/src/main/java/com/example/seniordesign/smartlighthub/HomePage.java
+++ app/src/main/java/com/example/seniordesign/smartlighthub/HomePage.java
@@ -1,6 +1,7 @@
 package com.example.seniordesign.smartlighthub;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,18 +9,43 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
     Button lightControls;
 
     Button musicControls;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() != null)
+                {
+                    Toast.makeText(HomePage.this, "Welcome", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
 
         lightControls = (Button) findViewById(R.id.lightControls);
         lightControls.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +71,14 @@ public class HomePage extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
@@ -60,6 +94,19 @@ public class HomePage extends AppCompatActivity {
             startActivity(new Intent(HomePage.this, SettingsPage.class));
         }
 
+        if (item.getItemId() == R.id.logoutButton)
+        {
+            logout();
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+    public void logout()
+    {
+        mAuth.signOut();
+        Toast.makeText(this, "User Logged Out", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(HomePage.this, MainActivity.class));
+    }
+
 }

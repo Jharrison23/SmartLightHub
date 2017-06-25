3a4093b1ffef7c11e70aac3f3513d999d0b7cb10
James Harrison <jharrison23@knights.ucf.edu>
2017-06-24 23:14:40 -0400

    Adding the preset adds it to the database

--- app/src/main/java/com/example/seniordesign/smartlighthub/View/AddPresetsActivity.java
+++ app/src/main/java/com/example/seniordesign/smartlighthub/View/AddPresetsActivity.java
@@ -1,6 +1,10 @@
 package com.example.seniordesign.smartlighthub.View;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +16,10 @@ import android.widget.Toast;
 
 import com.example.seniordesign.smartlighthub.Controller.BottomNavigation;
 import com.example.seniordesign.smartlighthub.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
 
@@ -34,9 +42,20 @@ public class AddPresetsActivity extends AppCompatActivity implements View.OnClic
     private Button thirdPageNext;
 
     private int pageNumber;
-
     private int defaultColor;
 
+    private String presetName;
+
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+
+    private FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    private DatabaseReference presetsRef = firebaseDatabase.getReference().child("Users").child(currentUser.getUid()).child("Presets");
 
 
 
@@ -47,7 +66,33 @@ public class AddPresetsActivity extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_presets);
 
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }
+        };
+
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null)
+        {
+            presetName = extras.getString("Preset Name");
+
+            Toast.makeText(this, presetName, Toast.LENGTH_SHORT).show();
+        }
         init();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
@@ -97,7 +142,6 @@ public class AddPresetsActivity extends AppCompatActivity implements View.OnClic
 
             pageNumber = 2;
 
-
         }
 
     }
@@ -110,8 +154,6 @@ public class AddPresetsActivity extends AppCompatActivity implements View.OnClic
         firstLightState = (Switch) findViewById(R.id.firstLightState);
         firstPageNext = (Button) findViewById(R.id.firstPageNext);
         firstPageNext.setOnClickListener(this);
-
-
 
         secondLightName  = (EditText) findViewById(R.id.secondLightName);
         secondLightColor = (ImageView) findViewById(R.id.secondLightColor);
@@ -152,9 +194,8 @@ public class AddPresetsActivity extends AppCompatActivity implements View.OnClic
                 thirdLightColor.setVisibility(View.INVISIBLE);
                 thirdLightState.setVisibility(View.INVISIBLE);
                 thirdPageNext.setVisibility(View.INVISIBLE);
+
                 pageNumber = 2;
-
-
                 break;
 
             case R.id.secondPageNext:
@@ -173,20 +214,17 @@ public class AddPresetsActivity extends AppCompatActivity implements View.OnClic
                 thirdLightColor.setVisibility(View.VISIBLE);
                 thirdLightState.setVisibility(View.VISIBLE);
                 thirdPageNext.setVisibility(View.VISIBLE);
+
                 pageNumber = 3;
-
-
 
                 break;
 
             case R.id.thirdPageNext:
-
+                savePreset();
                 startActivity(new Intent(AddPresetsActivity.this, BottomNavigation.class));
-
                 break;
 
             case R.id.firstLightColor:
-
                 openColorPickerDialog(false, firstLightColor);
                 break;
 
@@ -227,6 +265,67 @@ public class AddPresetsActivity extends AppCompatActivity implements View.OnClic
         });
 
         ambilWarnaDialog.show();
+
+    }
+
+
+    public void savePreset()
+    {
+        DatabaseReference currentPreset = presetsRef.child(presetName);
+
+        DatabaseReference firstLightPresetRef = currentPreset.child("Light 1");
+        firstLightPresetRef.child("Name").setValue(firstLightName.getText().toString());
+        Drawable firstLightDrawableColor = (Drawable) firstLightColor.getBackground();
+
+        int firstColorInt = ((ColorDrawable) firstLightDrawableColor).getColor();
+
+        int firstRed = Color.red(firstColorInt);                                                                                                                                                        startActivity(getIntent());
+        int firstGreen = Color.green(firstColorInt);
+        int firstBlue = Color.blue(firstColorInt);
+
+        String firstRGBcolor = firstRed + ", " + firstGreen + ", " + firstBlue;
+
+        firstLightPresetRef.child("Color").setValue(firstRGBcolor);
+        firstLightPresetRef.child("State").setValue(firstLightState.isChecked());
+
+
+
+        DatabaseReference secondLightPresetRef = currentPreset.child("Light 2");
+        secondLightPresetRef.child("Name").setValue(thirdLightName.getText().toString());
+        Drawable secondLightDrawableColor = (Drawable) secondLightColor.getBackground();
+
+        int secondColorInt = ((ColorDrawable) secondLightDrawableColor).getColor();
+
+        int secondRed = Color.red(secondColorInt);                                                                                                                                                        startActivity(getIntent());
+        int secondGreen = Color.green(secondColorInt);
+        int secondBlue = Color.blue(secondColorInt);
+
+        String secondRGBcolor = secondRed + ", " + secondGreen + ", " + secondBlue;
+
+
+        secondLightPresetRef.child("Color").setValue(secondRGBcolor);        
+        secondLightPresetRef.child("State").setValue(secondLightState.isChecked());
+
+
+
+        DatabaseReference thirdLightPresetRef = currentPreset.child("Light 3");
+        thirdLightPresetRef.child("Name").setValue(thirdLightName.getText().toString());
+        Drawable thirdLightDrawableColor = (Drawable) thirdLightColor.getBackground();
+
+        int thirdColorInt = ((ColorDrawable) thirdLightDrawableColor).getColor();
+
+        int thirdRed = Color.red(thirdColorInt);                                                                                                                                                        startActivity(getIntent());
+        int thirdGreen = Color.green(thirdColorInt);
+        int thirdBlue = Color.blue(thirdColorInt);
+
+        String thirdRGBcolor = thirdRed + ", " + thirdGreen + ", " + thirdBlue;
+
+        thirdLightPresetRef.child("Color").setValue(thirdRGBcolor);
+        thirdLightPresetRef.child("State").setValue(thirdLightState.isChecked());
+
+
+
+
 
     }
 
--- app/src/main/java/com/example/seniordesign/smartlighthub/View/LightPresets.java
+++ app/src/main/java/com/example/seniordesign/smartlighthub/View/LightPresets.java
@@ -1,14 +1,18 @@
 package com.example.seniordesign.smartlighthub.View;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.example.seniordesign.smartlighthub.R;
 
@@ -36,7 +40,41 @@ public class LightPresets extends Fragment {
         addPresets.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(), AddPresetsActivity.class));
+
+
+                final EditText input = new EditText(getActivity());
+
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+
+                input.setLayoutParams(lp);
+                AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+                alertDialog.setTitle("New Preset");
+                alertDialog.setView(input);
+                alertDialog.setMessage("Please Enter Preset Name");
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+
+                                Intent AddPresetIntent = new Intent(getActivity(), AddPresetsActivity.class);
+
+                                AddPresetIntent.putExtra("Preset Name", input.getText().toString());
+
+                                startActivity(AddPresetIntent);
+
+                            }
+                        });
+
+                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                alertDialog.show();
+
             }
         });
 

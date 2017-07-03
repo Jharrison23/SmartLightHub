9501e2ff696242689fe9cce66c5a8f1b8d0f5984
James Harrison <jharrison23@knights.ucf.edu>
2017-07-03 17:02:32 -0400

    Created the routine fragments and linked it to the bottom navigation bar created an add routine view which isn't connected yet
    Also whenever a user tries to create a preset, from mood or custom the light names are prefilled with the values of the current homepage light name so that the user doesnt have to update names every time but they can still change the name

--- app/src/main/java/com/example/seniordesign/smartlighthub/View/MoodCreation.java
+++ app/src/main/java/com/example/seniordesign/smartlighthub/View/MoodCreation.java
@@ -26,13 +26,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.seniordesign.smartlighthub.Controller.BottomNavigation;
+import com.example.seniordesign.smartlighthub.Model.Light;
 import com.example.seniordesign.smartlighthub.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MoodCreation extends AppCompatActivity implements View.OnClickListener{
 
@@ -50,8 +57,9 @@ public class MoodCreation extends AppCompatActivity implements View.OnClickListe
 
     private DatabaseReference presetsRef = firebaseDatabase.getReference().child("Users").child(currentUser.getUid()).child("Presets");
 
+    private DatabaseReference lightsRef = firebaseDatabase.getReference().child("Users").child(currentUser.getUid()).child("Lights");
 
-
+    private List<Light> presetLightList;
 
 
     private ConstraintLayout firstLightLayout;
@@ -76,7 +84,7 @@ public class MoodCreation extends AppCompatActivity implements View.OnClickListe
 
     private TextView lightNumberField;
 
-    private String presetName = "Test";
+    private String presetName;
 
 
 
@@ -98,7 +106,11 @@ public class MoodCreation extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_creation);
 
-        getSupportActionBar().setTitle("Create Mood");
+        if (getSupportActionBar() != null) {
+
+            getSupportActionBar().setTitle("Create Mood");
+
+        }
 
 
         mAuth = FirebaseAuth.getInstance();
@@ -200,6 +212,40 @@ public class MoodCreation extends AppCompatActivity implements View.OnClickListe
                 return true;
             }
         });
+
+        presetLightList = new ArrayList<>();
+
+        lightsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+
+                for (DataSnapshot child: children) {
+
+                    Light currentLight = child.getValue(Light.class);
+                    presetLightList.add(currentLight);
+
+                }
+
+                firstLightName.setText(presetLightList.get(0).getName());
+                secondLightName.setText(presetLightList.get(1).getName());
+                thirdLightName.setText(presetLightList.get(2).getName());
+
+            }
+
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+
+
+
+
 
 
 

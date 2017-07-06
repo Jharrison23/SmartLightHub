3568c2c65d2e4b2560cd5af95917f9efc5db1857
James Harrison <jharrison23@knights.ucf.edu>
2017-07-06 02:40:32 -0400

    Created the ui element for the routine row

--- app/src/main/java/com/example/seniordesign/smartlighthub/View/AddRoutine.java
+++ app/src/main/java/com/example/seniordesign/smartlighthub/View/AddRoutine.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,8 +26,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;

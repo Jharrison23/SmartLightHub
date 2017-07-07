64ada0b9ae79e64f6658113d0201dc4a786ba3f4
James Harrison <jharrison23@knights.ucf.edu>
2017-07-07 01:42:26 -0400

    implemented music mode, if you tap the music icon in the toolbar the light systems goes into music mode

--- app/src/main/java/com/example/seniordesign/smartlighthub/Controller/BottomNavigation.java
+++ app/src/main/java/com/example/seniordesign/smartlighthub/Controller/BottomNavigation.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CompoundButton;
@@ -26,6 +27,16 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.models.consumer.PNPublishResult;
+import com.pubnub.api.models.consumer.PNStatus;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class BottomNavigation extends AppCompatActivity {
 
@@ -135,6 +146,28 @@ public class BottomNavigation extends AppCompatActivity {
             startActivity(new Intent(BottomNavigation.this, SettingsPage.class));
         }
 
+        if (item.getItemId() == R.id.musicMode) {
+
+            JSONObject musicModeObject = new JSONObject();
+
+
+            try {
+                musicModeObject.put("0", 0);
+                musicModeObject.put("1", 0);
+                musicModeObject.put("2", 0);
+                musicModeObject.put("3", 0);
+                musicModeObject.put("4", 0);
+                musicModeObject.put("5", 1);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            pubnubConfig(musicModeObject);
+
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -166,5 +199,47 @@ public class BottomNavigation extends AppCompatActivity {
         Toast.makeText(this, "User Logged Out", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(this, MainActivity.class));
     }
+
+
+    public void pubnubConfig(final JSONObject pubnubObjects)
+    {
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.setSubscribeKey("sub-c-40e3d906-4ee7-11e7-bf50-02ee2ddab7fe");
+        pnConfiguration.setPublishKey("pub-c-6528095d-bc26-4768-a903-ac0a85174f81");
+        pnConfiguration.setSecure(false);
+
+        PubNub pubnub = new PubNub(pnConfiguration);
+
+                pubnub.publish().message(pubnubObjects).channel("Light_1")
+                        .async(new PNCallback<PNPublishResult>() {
+                            @Override
+                            public void onResponse(PNPublishResult result, PNStatus status) {
+                                Log.d("Music Mode ", "Light 1 publish: " + pubnubObjects);
+                            }
+                        });
+
+                pubnub.publish().message(pubnubObjects).channel("Light_2")
+                        .async(new PNCallback<PNPublishResult>() {
+                            @Override
+                            public void onResponse(PNPublishResult result, PNStatus status) {
+                                Log.d("Music Mode ", "Light 2 publish: " + pubnubObjects);
+                            }
+                        });
+
+                pubnub.publish().message(pubnubObjects).channel("Light_3")
+                        .async(new PNCallback<PNPublishResult>() {
+                            @Override
+                            public void onResponse(PNPublishResult result, PNStatus status) {
+                                Log.d("Music Mode ", "Light 3 publish: " + pubnubObjects);
+                            }
+                        });
+
+
+    }
+
+
+
+
+
 
 }
